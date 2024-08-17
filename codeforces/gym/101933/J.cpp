99138e7dfce78116e168bb48c1f687ebed452338
Van Nam Duong <z5412692@ad.unsw.edu.au>
2024-08-17 14:52:21 +1000

    adding J

--- codeforces/gym/101933/J.cpp
+++ codeforces/gym/101933/J.cpp
@@ -2,10 +2,12 @@
  *    author:  namduong93
  *    created: unknown
  *    complexity: unknown
+ *    test: 0 0 0 0, 1 1 1 0
  **/
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int long long
 #define fto(i, a, b) for (int i = a; i <= b; ++i)
 #define fdo(i, a, b) for (int i = a; i >= b; --i)
 #define mp make_pair
@@ -30,7 +32,6 @@ const int N = 300005;
 const int inf = 1e9 + 7;
 
 int n;
-int a[N];
 string str;
 string s[N];
 stack<int> st;
@@ -39,6 +40,66 @@ multiset<int> ms;
 
 void Solve()
 {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == 0 && b == 0 && c == 0 && d == 0)
+    {
+        cout << "0";
+        return;
+    }
+
+    int x = ceil(sqrt(a * 2));
+    int y = ceil(sqrt(d * 2));
+    if (x * (x - 1) / 2 != a)
+    {
+        cout << "impossible";
+        return;
+    }
+    if (y * (y - 1) / 2 != d)
+    {
+        cout << "impossible";
+        return;
+    }
+    if (a == 0 && (b >= 1 || c >= 1))
+    {
+        x = 1;
+    }
+
+    if (d == 0 && (b >= 1 || c >= 1))
+    {
+        y = 1;
+    }
+    if (x * y != b + c)
+    {
+        cout << "impossible";
+        return;
+    }
+    int cloneX = x;
+    int cloneY = y;
+    vector<int> v;
+    fto(i, 1, cloneX + cloneY)
+    {
+        if (b >= y)
+        {
+            v.push_back(0);
+            b -= y;
+        }
+        else
+        {
+            v.push_back(1);
+            y--;
+        }
+    }
+    if (b != 0)
+    {
+        cout << "impossible";
+        return;
+    }
+    if (v.size())
+        fto(i, 0, v.size() - 1)
+        {
+            cout << v[i];
+        }
 }
 
 int32_t main()
